531739ceea301f83f541ec0ce3197d2e10ea324f
casco <mitre3586>
2016-05-19 22:37:01 -0300

    ya marca y numera los pdfs

--- src/main/java/ar/org/sadio/Proceedingsator.java
+++ src/main/java/ar/org/sadio/Proceedingsator.java
@@ -18,37 +18,44 @@ import java.util.logging.Logger;
  */
 public class Proceedingsator {
     static String simposia = "Argentine Symposium on Artificial Intelligence";
-    static String acronym = "ASAI";
+    static String acronym = "ASAI 2015";
+    static String conference = "44jaiio";
     static String issn = "2451-7585";
     static String metaFile = "articles.csv";
     static String inputFolder = "example/input";
     static String outputFolder = "example/output";
     private Logger logger;
     List<Article> articles;
-    int pageNumber;
+    int lastUsedPageNumber;
 
     public Proceedingsator() {
         logger = Logger.getLogger(Proceedingsator.class.getName());
-        pageNumber = 1;
+        lastUsedPageNumber = 0;
         articles = new Vector<Article>();
         new File(outputFolder).mkdirs();
     }
 
-
     public static void main(String[] args) {
         Proceedingsator ator = new Proceedingsator();
         ator.readArticleList();
-
+        try {
+            ator.stampArticles();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
     }
 
-    private  void readArticleList() {
+
+    private void readArticleList() {
         Reader in = null;
         try {
             in = new FileReader("example/input/articles.csv");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        Iterable<CSVRecord>  records = null;
+        Iterable<CSVRecord> records = null;
         try {
             records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
         } catch (IOException e) {
@@ -61,37 +68,30 @@ public class Proceedingsator {
             articles.add(new Article(title, authors, file));
         }
         logger.info("Read " + articles.size() + " articles");
-        try {
-            stamp(articles.get(0));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (DocumentException e) {
-            e.printStackTrace();
+    }
+
+    private void stampArticles() throws IOException, DocumentException {
+        for (Article article : articles) {
+            stamp(article);
         }
     }
 
-    private void stamp (Article article) throws IOException, DocumentException {
-        PdfReader reader = null;
-        PdfStamper stamper = null;
-        reader = new PdfReader(inputFolder + "/" + article.getFilename());
-        stamper = new PdfStamper(reader, new FileOutputStream(outputFolder + "/" + article.getFilename()));
-
-        System.out.println(reader.getNumberOfPages() + " pages");
-
-        PdfContentByte under = stamper.getUnderContent(1);
-        Font f = new Font(Font.FontFamily.HELVETICA, 15);
-        Phrase p = new Phrase("This watermark is added UNDER the existing content", f);
-        ColumnText.showTextAligned(under, Element.ALIGN_CENTER, p, 297, 550, 0);
-        PdfContentByte over = stamper.getOverContent(1);
-        p = new Phrase("This watermark is added ON TOP OF the existing content", f);
-        ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 500, 0);
-        p = new Phrase("This TRANSPARENT watermark is added ON TOP OF the existing content", f);
-        over.saveState();
-        PdfGState gs1 = new PdfGState();
-        gs1.setFillOpacity(0.5f);
-        over.setGState(gs1);
-        ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 450, 0);
-        over.restoreState();
+    private void stamp(Article article) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(inputFolder + "/" + article.getFilename());
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(outputFolder + "/" + article.getFilename()));
+        int pages = reader.getNumberOfPages();
+        article.setStartPage(lastUsedPageNumber + 1);
+        article.setEndPage(article.getStartPage() + pages - 1);
+        lastUsedPageNumber = article.getEndPage();
+        for (int i = 0; i < pages; i++) {
+            PdfContentByte over = stamper.getOverContent(i + 1);
+            Font f = new Font(Font.FontFamily.HELVETICA, 8);
+            Phrase p = new Phrase(acronym + ", " + simposia, f);
+            ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 770, 0);
+            p = new Phrase(conference + " - " + acronym + " - ISSN: " +
+                     issn +" - Página " + (article.getStartPage() + i), f);
+            ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 50, 0);
+        }
         stamper.close();
         reader.close();
 
