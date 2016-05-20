b3b6d6b473a28b97b1df428cd70e7242fba1572c
casco <mitre3586>
2016-05-20 07:51:51 -0300

    refactored stamper

--- src/main/java/ar/org/sadio/Article.java
+++ src/main/java/ar/org/sadio/Article.java
@@ -1,5 +1,20 @@
 package ar.org.sadio;
 
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static ar.org.sadio.Proceedingsator.acronym;
+import static ar.org.sadio.Proceedingsator.simposia;
+
 /**
  * Created by alejandrofernandez on 5/19/16.
  */
@@ -7,13 +22,23 @@ public class Article {
     String authors;
     String title;
     String filename;
+    String location;
     int startPage;
     int endPage;
 
-    public Article(String title, String authors, String file) {
+    public Article(String title, String authors, String filename, String location) {
         this.authors = authors;
+        this.filename = filename;
+        this.location = location;
         this.title = title;
-        this.filename = file;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
 
@@ -55,5 +80,25 @@ public class Article {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    protected void stamp(String conference, String issn, int firstPage, String outputLocation) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(getLocation() + "/" + getFilename());
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(outputLocation + "/" + getFilename()));
+        int pages = reader.getNumberOfPages();
+        setStartPage(firstPage);
+        setEndPage(getStartPage() + pages - 1);
+        for (int i = 0; i < pages; i++) {
+            PdfContentByte over = stamper.getOverContent(i + 1);
+            Font f = new Font(Font.FontFamily.HELVETICA, 8);
+            Phrase p = new Phrase(acronym + ", " + simposia, f);
+            ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 770, 0);
+            p = new Phrase(conference + " - " + acronym + " - ISSN: " +
+                    issn +" - Página " + (getStartPage() + i), f);
+            ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 50, 0);
+        }
+        stamper.close();
+        reader.close();
+
     }
 }
--- src/main/java/ar/org/sadio/Proceedingsator.java
+++ src/main/java/ar/org/sadio/Proceedingsator.java
@@ -5,12 +5,8 @@ import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.*;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
 
 import java.io.*;
-import java.util.List;
-import java.util.Vector;
 import java.util.logging.Logger;
 
 /**
@@ -24,14 +20,14 @@ public class Proceedingsator {
     static String metaFile = "articles.csv";
     static String inputFolder = "example/input";
     static String outputFolder = "example/output";
+    Proceedings proceedings;
     private Logger logger;
-    List<Article> articles;
-    int lastUsedPageNumber;
+
+
 
     public Proceedingsator() {
         logger = Logger.getLogger(Proceedingsator.class.getName());
-        lastUsedPageNumber = 0;
-        articles = new Vector<Article>();
+        proceedings = new Proceedings(acronym, conference, issn, simposia);
         new File(outputFolder).mkdirs();
     }
 
@@ -47,6 +43,14 @@ public class Proceedingsator {
         }
     }
 
+    public String getInputFolder() {
+        return inputFolder;
+    }
+
+    private void stampArticles() throws IOException, DocumentException {
+        proceedings.stampArticles(outputFolder);
+    }
+
 
     private void readArticleList() {
         Reader in = null;
@@ -55,47 +59,12 @@ public class Proceedingsator {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        Iterable<CSVRecord> records = null;
-        try {
-            records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (CSVRecord record : records) {
-            String title = record.get("title");
-            String authors = record.get("authors");
-            String file = record.get("filename");
-            articles.add(new Article(title, authors, file));
-        }
-        logger.info("Read " + articles.size() + " articles");
+        proceedings.readArticleList(in, inputFolder);
     }
 
-    private void stampArticles() throws IOException, DocumentException {
-        for (Article article : articles) {
-            stamp(article);
-        }
-    }
 
-    private void stamp(Article article) throws IOException, DocumentException {
-        PdfReader reader = new PdfReader(inputFolder + "/" + article.getFilename());
-        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(outputFolder + "/" + article.getFilename()));
-        int pages = reader.getNumberOfPages();
-        article.setStartPage(lastUsedPageNumber + 1);
-        article.setEndPage(article.getStartPage() + pages - 1);
-        lastUsedPageNumber = article.getEndPage();
-        for (int i = 0; i < pages; i++) {
-            PdfContentByte over = stamper.getOverContent(i + 1);
-            Font f = new Font(Font.FontFamily.HELVETICA, 8);
-            Phrase p = new Phrase(acronym + ", " + simposia, f);
-            ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 770, 0);
-            p = new Phrase(conference + " - " + acronym + " - ISSN: " +
-                     issn +" - Página " + (article.getStartPage() + i), f);
-            ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 50, 0);
-        }
-        stamper.close();
-        reader.close();
 
-    }
+
 
 
 }
